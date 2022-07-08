19b817ca7b1f2cd3ba00a3cdbfe78ac517f16540
igwe-karl <kali4478@gmail.com>
2022-07-08 12:34:17 +0100

    first commit

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>god is good</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
